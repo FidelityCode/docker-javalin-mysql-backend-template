a855d696cbbcca00e19eddc46291f13383550922
Niklas Englmeier <niklas.englmeier@web.de>
2024-02-26 20:50:39 +0100

    Initial commit

---
+++ src/main/java/de/niklasenglmeier/backend/Main.java
@@ -0,0 +1,10 @@
+package de.niklasenglmeier.backend;
+
+import io.javalin.Javalin;
+
+public class Main {
+    public static void main(String[] args) {
+        Javalin app = Javalin.create();
+
+    }
+}
