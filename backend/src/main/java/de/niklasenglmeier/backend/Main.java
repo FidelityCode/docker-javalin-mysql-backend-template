715cbc23b0b2be92c8458f8db4f7f370cca1c146
iamniklas <niklas.englmeier@web.de>
2024-02-26 23:59:37 +0100

    Update Main.java

--- backend/src/main/java/de/niklasenglmeier/backend/Main.java
+++ backend/src/main/java/de/niklasenglmeier/backend/Main.java
@@ -4,9 +4,11 @@ import io.javalin.Javalin;
 
 public class Main {
     public static void main(String[] args) {
+        String env = System.getenv("DATABASE_HOST");
+
         Javalin app = Javalin
                 .create()
-                .get("/", ctx -> ctx.result("Hello World"));
+                .get("/", ctx -> ctx.result(env));
 
         app.start(9000);
     }
